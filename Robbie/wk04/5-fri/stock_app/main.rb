5409149b07a1ab1501aca52b9d372f9aa14e9593
Robbie <cprobbie23@gmail.com>
2017-12-26 17:38:56 +1100

    stock_app_1.1

--- Robbie/wk04/5-fri/stock_app/main.rb
+++ Robbie/wk04/5-fri/stock_app/main.rb
@@ -3,32 +3,60 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  i = 0 
   stock = StockQuote::Stock.quote("#{params[:stock_code]}")
-  @exchange = stock.e
-  @id = stock.id
-  @reuters = stock.f_reuters_url
-  @name = stock.name
-  @symb = stock.symbol
-  @price = stock.l
-  @cp = stock.cp
-  @c = stock.c
-  @hi52 = stock.hi52
-  @lo52 = stock.lo52
-  @market_cap = stock.mc
-  @beta = stock.beta
-  @dividend_yd = stock.dy
-  @shares = stock.shares
-  @pe = stock.pe
-  @eps = stock.eps
-  @ldiv = stock.ldiv
-  @instown = stock.instown
-  @sname = stock.sname
-  @iname = stock.iname
-  @open = stock.op
-  @hi = stock.hi
-  @lo = stock.lo
-  @vo = stock.vo
-  @avvo = stock.avvo
-  erb(:index)
-
+  @exchange = stock[i].e
+  @id = stock[i].id
+  @reuters = stock[i].f_reuters_url
+  @name = stock[i].name
+  @symb = stock[i].symbol
+  @price = stock[i].l
+  @cp = stock[i].cp
+  @c = stock[i].c
+  @hi52 = stock[i].hi52
+  @lo52 = stock[i].lo52
+  @market_cap = stock[i].mc
+  @beta = stock[i].beta
+  @dividend_yd = stock[i].dy
+  @shares = stock[i].shares
+  @pe = stock[i].pe
+  @eps = stock[i].eps
+  @ldiv = stock[i].ldiv
+  @instown = stock[i].instown
+  @sname = stock[i].sname
+  @iname = stock[i].iname
+  @open = stock[i].op
+  @hi = stock[i].hi
+  @lo = stock[i].lo
+  @vo = stock[i].vo
+  @avvo = stock[i].avvo
+  
+  i = 1 
+  stock = StockQuote::Stock.quote("#{params[:stock_code]}")
+  @exchange1 = stock[i].e
+  @id1 = stock[i].id
+  @reuters1 = stock[i].f_reuters_url
+  @name1 = stock[i].name
+  @symb1 = stock[i].symbol
+  @price1 = stock[i].l
+  @cp1 = stock[i].cp
+  @c1 = stock[i].c
+  @hi521 = stock[i].hi52
+  @lo521 = stock[i].lo52
+  @market_cap1 = stock[i].mc
+  @beta1 = stock[i].beta
+  @dividend_yd1 = stock[i].dy
+  @shares1 = stock[i].shares
+  @pe1 = stock[i].pe
+  @eps1 = stock[i].eps
+  @ldiv1 = stock[i].ldiv
+  @instown1 = stock[i].instown
+  @sname1 = stock[i].sname
+  @iname1 = stock[i].iname
+  @open1 = stock[i].op
+  @hi1 = stock[i].hi
+  @lo1 = stock[i].lo
+  @vo1 = stock[i].vo
+  @avvo1 = stock[i].avvo
+  erb(:index) 
 end
--- Robbie/wk04/5-fri/stock_app/public/app.js
+++ Robbie/wk04/5-fri/stock_app/public/app.js
@@ -1,5 +1,7 @@
 var change = document.querySelector('#chg')
+var change1 = document.querySelector('#chg1')
 var changeP = document.querySelector('#chgP')
+var changeP1 = document.querySelector('#chgP1')
 
 if (Number(change.textContent) > 0 ) {
   document.querySelector(".right p").style.color = "green"
@@ -10,4 +12,15 @@ if (Number(changeP.textContent) > 0 ) {
   change.style.color = "green"
 } else if (Number(changeP.textContent) < 0 ) {
   change.style.color = "red"
+}
+
+if (Number(change1.textContent) > 0 ) {
+  document.querySelector("#p1").style.color = "green"
+} else if (Number(change1.textContent) < 0 ) {
+  document.querySelector("#p1").style.color = "red"
+}
+if (Number(changeP1.textContent) > 0 ) {
+  change1.style.color = "green"
+} else if (Number(changeP1.textContent) < 0 ) {
+  change1.style.color = "red"
 }
