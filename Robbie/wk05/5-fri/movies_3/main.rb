be8045d675bd4e6e684ffd238ddc7fb4468c454e
Robbie <cprobbie23@gmail.com>
2018-01-14 10:43:55 +1100

    built_a_basic_functional_app

---
+++ Robbie/wk05/5-fri/movies_3/main.rb
@@ -0,0 +1,39 @@
+     
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+
+get '/' do
+  # show search history
+  # conn = PG.connect(dbname: 'goodfoodhunting')
+  # sql = "SELECT title FROM movies LIMIT 10;"
+  # @movies = conn.exec(sql)
+  # conn.close
+  erb :index
+end
+
+get '/search_results' do
+  @movie_keyword = "#{params[:movie_name]}"
+  result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&s=#{params[:movie_name]}")
+  @result_hash = result.parsed_response["Search"]
+
+  if @result_hash.count == 1
+    result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{params[:movie_name]}")
+    @result_hash = result.parsed_response
+    erb :movie
+  else
+  erb :search_results
+  end
+end
+
+get '/movie' do
+  # if title exists, diplay from database
+  # if not exists, insert movie into database
+
+  result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&i=#{params[:i]}")
+  @result_hash = result.parsed_response
+  # conn = PG.connect(dbname: 'movie_db')
+  erb :movie
+end
+
