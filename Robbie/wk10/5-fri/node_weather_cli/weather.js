98e103a89bb3fd63ccb0efd58d0031571a343df5
Robbie <cprobbie23@gmail.com>
2018-02-16 16:00:03 +1100

    hide api key

--- Robbie/wk10/5-fri/node_weather_cli/weather.js
+++ Robbie/wk10/5-fri/node_weather_cli/weather.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var city = process.argv[2]
-var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d2a5832ea33005167572b1a51000b744`
+var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.OPENWEATHER_KEY}`
 request(url, function (error,response,body) {
   var absTemp = JSON.parse(body).main.temp;
   var celTemp = (absTemp - 273.15).toFixed(1);
