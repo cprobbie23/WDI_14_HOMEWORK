31609a0ff1222803b4fd9c5735e685155121a255
Robbie <cprobbie23@gmail.com>
2018-02-17 11:35:21 +1100

    finish test_string_representation

--- Robbie/wk10/2-tue/queen_attack/queens.rb
+++ Robbie/wk10/2-tue/queen_attack/queens.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Queens
   def initialize(*args)
     @args
@@ -20,9 +21,28 @@ class Queens
     @b_queen_pos
   end
 
-  def to_s
-    
-    
-  end
+  # def to_s
+  #   board = Array.new(8)
+  #   8.times do |row_index|
+  #     board[row_index] = ("O " * 8).chomp(' ')
+  #   end
+  #   board.join("\n")
+  # end
 
+
+  def to_s
+    board = Array.new(8)
+    8.times do |row_index|
+      board[row_index] = Array.new(8)
+      8.times do |col_index|
+          board[row_index][col_index] = "O"
+      end
+    end
+    board[@w_queen_pos[0]][@w_queen_pos[1]] = "W"
+    board[@b_queen_pos[0]][@b_queen_pos[1]] = "B"
+    new_board = board.map do |row|
+      row.join(' ')
+    end
+    new_board.join("\n")
+  end
 end
